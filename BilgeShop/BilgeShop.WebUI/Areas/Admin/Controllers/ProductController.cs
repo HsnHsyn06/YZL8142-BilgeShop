87ab0680181bdf3d57dcc513ff331d16c860ed6d
HsnHsyn06 <127850756+HsnHsyn06@users.noreply.github.com>
2023-03-14 14:48:07 +0300

    Son hal.

---
+++ BilgeShop/BilgeShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BilgeShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
